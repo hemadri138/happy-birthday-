5b4061387ea7f9908427a5f69c3a61d8951865a4
hemadri <hemadrikurukuti.202ms015@nitk.edu.in>
2023-04-03 11:52:30 +0530

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+  <p>&copy; 2023 - Happy Birthday Divya</p>
+  <p>Made with love by Manju</p>
+</footer>
---
+++ header.php
@@ -0,0 +1,4 @@
+<header>
+  <h1>Happy Birthday Divya!</h1>
+  <p>Made with love by Manju</p>
+</header>
